3abbbcf52bf567c2898b11578e67b31f0c20ea0e
KR-SANU-613 <117252804+KR-SANU-613@users.noreply.github.com>
2022-11-27 11:30:30 +0530

    Time: 23 ms (7.43%), Space: 57.7 MB (49.12%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean searchMatrix(int[][] nums, int target) {
+                int l=nums.length;
+        Boolean result=false;
+        for(int i=0 ; i<l ; i++){
+            for(int j=0; j<nums[i].length; j++){
+            if(nums[i][j]==target){
+                result=true;
+                break;
+            }}}
+        
+        return result;
+    }
+}
